0b678c00de2d83e481ce4551778feaf572be2596
zali14 <zali14@mail.depaul.edu>
2017-10-03 18:09:35 +0000

    Complete advanced setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'my_pages/home'
-
-  get 'my_pages/help'
-
-  get 'my_pages/about'
-
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
